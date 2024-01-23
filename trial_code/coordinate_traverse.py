c2b558e62162657a74bab09107c31ea985043c3c
Sunwoo Kim <suwnoo604@gmail.com>
2024-01-22 23:15:05 -0800

    updated angle calculation

--- trial_code/coordinate_traverse.py
+++ trial_code/coordinate_traverse.py
@@ -73,14 +73,22 @@ def unique_coordinates(start,ang,length,interval = 0.00005):
         prev_img = temp_img
     return valid_co
 
+#ang: the angle of the road in radian
+#inc: the anticipated heading compared to the road direction
 def ang_add(ang, inc):
-    out = (ang*180/np.pi)+inc
+    ang_deg = (ang*180/np.pi)
+    if ang_deg > 90:
+        ang_deg = 450 - ang_deg
+    else:
+        ang_deg = 90 - ang_deg
+    out = ang_deg +inc
     if out >360:
         out -= 360
     elif out <0:
         out+360
     return out
-#%%
+
+#loc1 and loc2 should be string
 def traverse_collect_images(loc1,loc2):
     one = loc1.split(',')
     one[0] = float(one[0])
@@ -106,7 +114,34 @@ def traverse_collect_images(loc1,loc2):
             with open('../temp/test'+str(i)+'_'+str(h)+'.jpg', 'wb') as file:
                 file.write(img.content)
 
+#sample code, provided loc1, loc2. This just to show the code work. 
+#The heading is set to 0 which to see if the heading is following the road.
+"""
+loc1='32.85179322509682,-117.19691677340164'
+loc2='32.8514507286907,-117.19493193834228'
+one = loc1.split(',')
+one[0] = float(one[0])
+one[1] = float(one[1])
+two = loc2.split(',')
+two[0] = float(two[0])
+two[1] = float(two[1])
+#first number indicates y coordinate and second number indicate x coordinate
+ygap = two[0] - one[0]
+xgap = two[1] - one[1]
+length = (xgap**2+ygap**2)**0.5
+ang = np.arctan2(ygap,xgap)
+coors = unique_coordinates(one,ang,length)
+leftside = [-150,-90,-30]
+rightside = [150,90,30]
+for i,c in enumerate(coors):
+    img = get_pic(loc = float_to_co(c),heading = ang_add(ang,0))
+    with open('../temp/test'+str(i)+'.jpg', 'wb') as file:
+        file.write(img.content)
+"""
+#sample code 2
+"""
+loc1='32.85179322509682,-117.19691677340164'
+loc2='32.8514507286907,-117.19493193834228'
+traverse_collect_images(loc1,loc2)
+"""
 # %%
-        
-#loc1='32.85179322509682,-117.19691677340164'
-#loc2='32.8514507286907, -117.19493193834228'
