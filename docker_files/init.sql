b047605790e64eb76af1ec671ee333c25c2a88af
Kevin Bui <k5bui@ucsd.edu>
2024-01-24 17:58:10 -0800

    added material type attribute to poles table

--- docker_files/init.sql
+++ docker_files/init.sql
@@ -3,9 +3,10 @@
 CREATE TABLE poles (
     id SERIAL PRIMARY KEY,
     latitude DOUBLE PRECISION,
-    longitude DOUBLE PRECISION
+    longitude DOUBLE PRECISION,
+    type VARCHAR(6)
 );
 
-INSERT INTO poles (latitude, longitude) VALUES
-    (32.7455211, -117.0242656),
-    (32.745515, -117.023937);
+INSERT INTO poles (latitude, longitude, type) VALUES
+    (32.7455211, -117.0242656, 'Metal'),
+    (32.745515, -117.023937, 'Wooden');
