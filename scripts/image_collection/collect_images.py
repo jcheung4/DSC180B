614b3553b94a2177ab3fc9ced272d9bdf85533a5
Kevin Bui <k5bui@ucsd.edu>
2024-03-14 11:08:33 -0700

    updated collect_image script to reference correct directory

--- scripts/image_collection/collect_images.py
+++ scripts/image_collection/collect_images.py
@@ -1,9 +1,9 @@
 import images_individual as ii
 
-OUTPATH = '../../data/images/'
+OUTPATH = './data/images/'
 
 if __name__== "__main__":
-    ii.images_kevin('../../data/kevin_structures.json', OUTPATH)
-    ii.images_sunny('../../data/sunny_structure_coordinates.json', OUTPATH)
-    ii.images_jonathan('../../data/jonathan_structures.json', OUTPATH)
+    ii.images_kevin('./data/kevin_structures.json', OUTPATH)
+    ii.images_sunny('./data/sunny_structure_coordinates.json', OUTPATH)
+    ii.images_jonathan('./data/jonathan_structures.json', OUTPATH)
     ii.images_derek()
